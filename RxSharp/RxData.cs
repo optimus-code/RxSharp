e178456a1830cd8229aa22ce05e33105372e98f8
Dan <12039578+optimus-code@users.noreply.github.com>
2024-08-26 17:49:41 +0100

    Correct accessability mistake

--- RxSharp/RxData.cs
+++ RxSharp/RxData.cs
@@ -7,52 +7,52 @@ namespace RxSharp
 {
     public static class RxData
     {
-        private static RpgSystem LoadSystem( string file )
+        public static RpgSystem LoadSystem( string file )
         {
             return LoadSingle<RpgSystem>( file );
         }
 
-        private static void Save( string file, RpgSystem instance )
+        public static void Save( string file, RpgSystem instance )
         {
             SaveSingle( file, instance );
         }
 
-        private static Dictionary<int, MapInfo> LoadMapInfos( string file )
+        public static Dictionary<int, MapInfo> LoadMapInfos( string file )
         {
             return LoadDictionary<MapInfo>( file );
         }
 
-        private static void Save( string file, Dictionary<int, MapInfo> instance )
+        public static void Save( string file, Dictionary<int, MapInfo> instance )
         {
             SaveDictionary( file, instance );
         }
 
-        private static Map LoadMap( string file )
+        public static Map LoadMap( string file )
         {
             return LoadSingle<Map>( file );
         }
 
-        private static void Save( string file, Map instance )
+        public static void Save( string file, Map instance )
         {
             SaveSingle( file, instance );
         }
 
-        private static List<Actor> LoadActors( string file )
+        public static List<Actor> LoadActors( string file )
         {
             return LoadList<Actor>( file );
         }
 
-        private static void Save( string file, List<Actor> instance )
+        public static void Save( string file, List<Actor> instance )
         {
             SaveList( file, instance );
         }
 
-        private static List<Animation> LoadAnimations( string file )
+        public static List<Animation> LoadAnimations( string file )
         {
             return LoadList<Animation>( file );
         }
 
-        private static void Save( string file, List<Animation> instance )
+        public static void Save( string file, List<Animation> instance )
         {
             SaveList( file, instance );
         }
@@ -62,107 +62,107 @@ namespace RxSharp
             return LoadList<Armor>( file );
         }
 
-        private static void Save( string file, List<Armor> instance )
+        public static void Save( string file, List<Armor> instance )
         {
             SaveList( file, instance );
         }
 
-        private static List<Class> LoadClasses( string file )
+        public static List<Class> LoadClasses( string file )
         {
             return LoadList<Class>( file );
         }
 
-        private static void Save( string file, List<Class> instance )
+        public static void Save( string file, List<Class> instance )
         {
             SaveList( file, instance );
         }
 
-        private static List<CommonEvent> LoadCommonEvents( string file )
+        public static List<CommonEvent> LoadCommonEvents( string file )
         {
             return LoadList<CommonEvent>( file );
         }
 
-        private static void Save( string file, List<CommonEvent> instance )
+        public static void Save( string file, List<CommonEvent> instance )
         {
             SaveList( file, instance );
         }
 
-        private static List<Enemy> LoadEnemies( string file )
+        public static List<Enemy> LoadEnemies( string file )
         {
             return LoadList<Enemy>( file );
         }
 
-        private static void Save( string file, List<Enemy> instance )
+        public static void Save( string file, List<Enemy> instance )
         {
             SaveList( file, instance );
         }
 
-        private static List<Item> LoadItems( string file )
+        public static List<Item> LoadItems( string file )
         {
             return LoadList<Item>( file );
         }
 
-        private static void Save( string file, List<Item> instance )
+        public static void Save( string file, List<Item> instance )
         {
             SaveList( file, instance );
         }
 
-        private static List<Script> LoadScripts( string file )
+        public static List<Script> LoadScripts( string file )
         {
             return LoadList<Script>( file );
         }
 
-        private static void Save( string file, List<Script> instance )
+        public static void Save( string file, List<Script> instance )
         {
             SaveList( file, instance );
         }
 
-        private static List<Skill> LoadSkills( string file )
+        public static List<Skill> LoadSkills( string file )
         {
             return LoadList<Skill>( file );
         }
 
-        private static void Save( string file, List<Skill> instance )
+        public static void Save( string file, List<Skill> instance )
         {
             SaveList( file, instance );
         }
 
-        private static List<State> LoadStates( string file )
+        public static List<State> LoadStates( string file )
         {
             return LoadList<State>( file );
         }
 
-        private static void Save( string file, List<State> instance )
+        public static void Save( string file, List<State> instance )
         {
             SaveList( file, instance );
         }
 
-        private static List<Tileset> LoadTilesets( string file )
+        public static List<Tileset> LoadTilesets( string file )
         {
             return LoadList<Tileset>( file );
         }
 
-        private static void Save( string file, List<Tileset> instance )
+        public static void Save( string file, List<Tileset> instance )
         {
             SaveList( file, instance );
         }
 
-        private static List<Troop> LoadTroops( string file )
+        public static List<Troop> LoadTroops( string file )
         {
             return LoadList<Troop>( file );
         }
 
-        private static void Save( string file, List<Troop> instance )
+        public static void Save( string file, List<Troop> instance )
         {
             SaveList( file, instance );
         }
 
-        private static List<Weapon> LoadWeapons( string file )
+        public static List<Weapon> LoadWeapons( string file )
         {
             return LoadList<Weapon>( file );
         }
 
-        private static void Save( string file, List<Weapon> instance )
+        public static void Save( string file, List<Weapon> instance )
         {
             SaveList( file, instance );
         }
